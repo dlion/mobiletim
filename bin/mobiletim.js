04f4bd55d5253b8971622f6ebd19818404c9e3e7
Domenico Luciani <domenicoleoneluciani@gmail.com>
2015-06-18 01:48:14 +0200

    Style

--- bin/mobiletim.js
+++ bin/mobiletim.js
@@ -3,31 +3,32 @@ var request = require('request'),
     colors  = require('colors/safe');
 
 request({
-  url: "https://www.tim.it/authfe/loginTIMinternet.do?urlOk=https://www.119selfservice.tim.it/area-clienti-119/rest/promozioniAttivePP/json",
+  url: "https://www.tim.it/authfe/loginTIMinternet.do?urlOk=" +
+        "https://www.119selfservice.tim.it/area-clienti-119/rest/promozioniAttivePP/json",
   method: 'POST',
   jar: true
-}, function(err, res, req) {
-  if(!err) {
+}, function (err, res, req) {
+  if (!err) {
     request({
       url: "https://www.119selfservice.tim.it/area-clienti-119/rest/promozioniAttivePP/json",
       method: 'POST',
       json: true,
       jar: true,
       strictSSL: false
-    }, function(err, res, req) {
-      var i, j;
-      if(!err) {
-        for(i in req.promozioniList) {
-          console.log("-- "+colors.green(req.promozioniList[i].promotionName)+" --");
-          for(j in req.promozioniList[i].caratteristiche) {
-            console.log(colors.blue(req.promozioniList[i].caratteristiche[j].descrizione)+ "-- End Validity: "+colors.red(req.promozioniList[i].caratteristiche[j].endvalidity));
+    }, function (err, res, req) {
+      if (!err) {
+        for(var i in req.promozioniList) {
+          console.log("-- " + colors.green(req.promozioniList[i].promotionName) + " --");
+          for(var j in req.promozioniList[i].caratteristiche) {
+            console.log(colors.blue(req.promozioniList[i].caratteristiche[j].descrizione) + "--" +
+                        "End Validity: " + colors.red(req.promozioniList[i].caratteristiche[j].endvalidity));
           }
         }
       } else {
-        console.log('Error: '+err);
+        console.log('Error: ' + err);
       }
     });
   } else {
-    console.log('Error: '+err);
+    console.log('Error: ' + err);
   }
 });
